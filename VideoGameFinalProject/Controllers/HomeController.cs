89db2aaf66a9ff39aa778bd98a0568f96c8c4539
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2021-04-19 16:17:35 -0400

    Added Nintendo Switch page by Sukhbir and Jaspreet

--- VideoGameFinalProject/Controllers/HomeController.cs
+++ VideoGameFinalProject/Controllers/HomeController.cs
@@ -48,6 +48,11 @@ namespace VideoGameFinalProject.Controllers
             return View();
         }
 
+        public IActionResult NintendoSwitch()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
